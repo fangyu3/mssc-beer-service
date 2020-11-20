98bd4dec9080867441d5e076a22e0dd123066b6e
fangyu3 <tofang23@gmail.com>
2020-11-20 01:28:44 -0500

    Adding Beer Repository. closes #5

---
+++ src/main/java/com/fangyu3/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.fangyu3.msscbeerservice.repositories;
+
+
+import com.fangyu3.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
